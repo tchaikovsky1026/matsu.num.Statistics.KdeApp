11f4211d71e628de548afd86a65deae4e1975391
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-01-13 13:58:55 +0900

    add: module-info

---
+++ src/module-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * カーネル密度推定の実行 App.
+ * 
+ * <p>
+ * <i>依存モジュール:</i> <br>
+ * {@code matsu.num.Statistics.KernelDensity}
+ * </p>
+ * 
+ * @author Matsuura Y.
+ * @version 0.1.0
+ */
+module matsu.num.Statistics.KdeApp {
+    requires matsu.num.Statistics.KernelDensity;
+}
