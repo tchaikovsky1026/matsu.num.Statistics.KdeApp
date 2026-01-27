835883c8e2f61dccd51f0a5ae9986a3e5a423e31
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-01-27 23:32:19 +0900

    feat.test: WritingFormatterのラベルヘッダに文字列を指定できるようにする (#8)

--- test/matsu/num/statistics/kdeapp/kde1d/WritableKde1dResultTest.java
+++ test/matsu/num/statistics/kdeapp/kde1d/WritableKde1dResultTest.java
@@ -76,7 +76,7 @@ final class WritableKde1dResultTest {
         }
     }
 
-    public static class 結果出力のフォーマットに関する_ラベル有り {
+    public static class 結果出力のフォーマットに関する_ラベル有り_char指定 {
 
         private final char separator = ',';
         private final char labelHeader = '/';
@@ -121,6 +121,65 @@ final class WritableKde1dResultTest {
 
             // 最初の行はヘッダ
             assertThat(ite.next().charAt(0), is(labelHeader));
+
+            // 2行目以降はデータ
+            for (; ite.hasNext();) {
+                String s = ite.next();
+                String[] values = s.split(String.valueOf(separator));
+                assertThat(values.length, is(2));
+
+                // 例外がスローされないことを確かめる
+                Double.parseDouble(values[0]);
+                Double.parseDouble(values[1]);
+            }
+        }
+    }
+
+    public static class 結果出力のフォーマットに関する_ラベル有り_String指定 {
+
+        private final char separator = ',';
+        private final String labelHeader = "//";
+
+        private WritableKde1dResult result;
+        private WritingFormatter formatter;
+
+        @Before
+        public void before_結果を準備() {
+            result = new GaussianStandardKde1dCalculator()
+                    .calc(new double[] { 0d, 1d });
+        }
+
+        @Before
+        public void before_フォーマッターの準備() {
+            // カンマ区切り, スラッシュがラベル
+            formatter = new WritingFormatter.Builder()
+                    .setSeparator(separator)
+                    .enableLabel(labelHeader)
+                    .build();
+        }
+
+        @Test
+        public void test_結果は1件以上() {
+            String[] lineStrings = resultToStrings(result, formatter);
+
+            int length = (int) Arrays.stream(lineStrings)
+                    .filter(s -> !s.isBlank())
+                    .count();
+
+            // ラベル行を抜いたlength
+            length--;
+
+            assertThat(length, is(greaterThan(0)));
+        }
+
+        @Test(expected = None.class)
+        public void test_結果の検証() {
+            String[] lineStrings = resultToStrings(result, formatter);
+
+            Iterator<String> ite = Arrays.stream(lineStrings).iterator();
+
+            // 最初の行はヘッダ
+            assertThat(ite.next().startsWith(labelHeader), is(true));
 
             // 2行目以降はデータ
             for (; ite.hasNext();) {
