38fd049f5df96a00ba3f5f7d7d9b94023e552227
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-02-06 17:10:02 +0900

    feat,test: SeparatorInterpreterについて, 区切り文字の条件を整備

--- src/matsu/num/statistics/kdeapp/kde1d/command/SeparatorInterpreter.java
+++ src/matsu/num/statistics/kdeapp/kde1d/command/SeparatorInterpreter.java
@@ -10,16 +10,23 @@
  */
 package matsu.num.statistics.kdeapp.kde1d.command;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * 区切り文字の解釈を行うクラス.
  * 
  * <p>
- * ルールは次の通りである.
+ * ルールは次の通りである. <br>
+ * エスケープシーケンスは, 列挙定数で用意されている.
  * </p>
  * 
  * <ul>
  * <li>ASCII 1文字</li>
- * <li>エスケープ文字</li>
+ * <li>エスケープシーケンス</li>
  * </ul>
  * 
  * @author Matsuura Y.
@@ -40,10 +47,87 @@ final class SeparatorInterpreter {
      * @throws NullPointerException 引数がnull
      */
     static Character from(String s) {
-        if (s.length() == 0) {
-            return null;
+
+        if (s.length() == 1) {
+            // ASCII かどうかを判定して返す.
+            char c = s.charAt(0);
+            return c <= 0x7F ? c : null;
         }
 
-        return s.charAt(0);
+        // バックスラッシュ1文字はここまで到達しない
+        if (s.startsWith("\\")) {
+            return EscapeSequence.from(s);
+        }
+
+        return null;
+    }
+
+    /**
+     * エスケープシーケンスは, バックスラッシュで開始されることを前提とする.
+     * 
+     * @author Matsuura Y.
+     */
+    static enum EscapeSequence {
+
+        /**
+         * タブ.
+         */
+        TAB("\\t", '\t'),
+
+        /**
+         * キャリッジリターン.
+         */
+        CR("\\r", '\r'),
+
+        /**
+         * ラインフィード.
+         */
+        LF("\\n", '\n'),
+
+        /**
+         * バックスラッシュ.
+         */
+        BACKSLASH("\\\\", '\\');
+
+        private final String stringValue;
+        private final char charValue;
+
+        private EscapeSequence(String stringValue, char charValue) {
+            this.stringValue = stringValue;
+            this.charValue = charValue;
+        }
+
+        String stringValue() {
+            return stringValue;
+        }
+
+        char charValue() {
+            return charValue;
+        }
+
+        /**
+         * 文字列をエスケープシーケンスとして文字を取得する.
+         * 
+         * @param s 文字列
+         * @return 文字, 該当する物がない場合はnull
+         * @throws NullPointerException 引数がnull
+         */
+        static Character from(String s) {
+            return MapperHolder.mapper.get(
+                    Objects.requireNonNull(s));
+        }
+
+        /**
+         * String から char へのマッパホルダ.
+         */
+        private static final class MapperHolder {
+
+            static final Map<String, Character> mapper;
+
+            static {
+                mapper = Arrays.stream(EscapeSequence.values())
+                        .collect(toMap(EscapeSequence::stringValue, EscapeSequence::charValue));
+            }
+        }
     }
 }
