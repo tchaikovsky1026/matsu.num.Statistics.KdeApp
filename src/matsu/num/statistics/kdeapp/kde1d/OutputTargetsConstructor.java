e4c7e527f8f98e131f41cf298eea2b6fb033a81a
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-02-06 20:47:36 +0900

    feat: 出力ターゲットを指定する仕組みを検討

---
+++ src/matsu/num/statistics/kdeapp/kde1d/OutputTargetsConstructor.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2026.2.6
+ */
+package matsu.num.statistics.kdeapp.kde1d;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.Objects;
+
+import matsu.num.statistics.kdeapp.kde1d.command.ConsoleParameterInterpreter;
+
+/**
+ * {@link OutputTargets} の構築器.
+ * 
+ * @author Matsuura Y.
+ */
+final class OutputTargetsConstructor implements ComponentConstructor<OutputTargets> {
+
+    private final PrintStream out;
+    private final PrintStream err;
+
+    /**
+     * 唯一のコンストラクタ.
+     * 標準入出力を与える.
+     */
+    OutputTargetsConstructor(PrintStream out, PrintStream err) {
+        this.out = Objects.requireNonNull(out);
+        this.err = Objects.requireNonNull(err);
+    }
+
+    /**
+     * @throws InvalidParameterException {@inheritDoc }
+     * @throws NullPointerException {@inheritDoc }
+     */
+    @Override
+    public OutputTargets construct(ConsoleParameterInterpreter interpreter) {
+        // 暫定: オプションからは何も受け取らない
+        Objects.requireNonNull(interpreter);
+        return new OutputTargets(out, err, List.of());
+    }
+}
