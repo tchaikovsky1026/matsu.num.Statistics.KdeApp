10ed3981716bbf3d8e72161778e9083e89a3289e
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-02-17 15:43:24 +0900

    test: fix ArgumentRequiringCommand(kde1d,kde2d)

--- test/matsu/num/statistics/kdeapp/kde1d/command/ArgumentRequiringCommandTest.java
+++ test/matsu/num/statistics/kdeapp/kde1d/command/ArgumentRequiringCommandTest.java
@@ -8,17 +8,18 @@
 package matsu.num.statistics.kdeapp.kde1d.command;
 
 import static matsu.num.statistics.kdeapp.kde1d.command.ArgumentRequiringCommand.*;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-
-import matsu.num.statistics.kdeapp.kde1d.exception.InvalidParameterException;
 
 /**
  * {@link ArgumentRequiringCommand} のテスト.
@@ -26,29 +27,45 @@ import matsu.num.statistics.kdeapp.kde1d.exception.InvalidParameterException;
 @RunWith(Enclosed.class)
 final class ArgumentRequiringCommandTest {
 
+    public static final Class<?> TEST_CLASS = ArgumentRequiringCommand.class;
+
     public static class オプションコマンドの集合生成に関するテスト {
 
         @Test
-        public void test_INPUT_FILE_PATHを含むことを確かめる() {
-            assertThat(values(), containsInRelativeOrder(INPUT_FILE_PATH));
+        public void test_サイズが1以上を確かめ文字列列挙() {
+            assertThat(values().size(), is(greaterThan(0)));
+
+            System.out.println(TEST_CLASS.getName());
+            values().stream()
+                    .forEach(System.out::println);
+            System.out.println();
         }
     }
 
-    public static class オプションコマンドの文字列解釈のテスト {
+    @RunWith(Theories.class)
+    public static class オプションコマンドの文字列解釈の正常系テスト {
+
+        @DataPoints
+        public static Collection<ArgumentRequiringCommand<?>> commands = values();
+
+        @Theory
+        public void test_各コマンドについて解釈を実行(ArgumentRequiringCommand<?> command) {
+            assertThat(
+                    interpret(command.commandString()).get(),
+                    is(command));
+            for (String commandStr : command.expressions()) {
+                assertThat(
+                        interpret(commandStr).get(),
+                        is(command));
+            }
+        }
+    }
+
+    public static class オプションコマンドの文字列解釈の異常系テスト {
 
         @Test(expected = NullPointerException.class)
-        public void test_nullは不可() {
+        public void test_nullの場合() {
             interpret(null);
-        }
-
-        @Test
-        public void test_セパレータコマンド() {
-            assertThat(
-                    interpret("--separator").get(),
-                    is(SEPARATOR));
-            assertThat(
-                    interpret("-sep").get(),
-                    is(SEPARATOR));
         }
 
         @Test
@@ -56,14 +73,6 @@ final class ArgumentRequiringCommandTest {
             assertThat(
                     interpret("--unknown"),
                     is(Optional.empty()));
-        }
-    }
-
-    public static class オプションコマンドのコンバータのバリデーションテスト {
-
-        @Test(expected = InvalidParameterException.class)
-        public void test_セパレータは空文字は不可() {
-            SEPARATOR.convertArg("");
         }
     }
 }
--- test/matsu/num/statistics/kdeapp/kde2d/command/ArgumentRequiringCommandTest.java
+++ test/matsu/num/statistics/kdeapp/kde2d/command/ArgumentRequiringCommandTest.java
@@ -11,13 +11,15 @@ import static matsu.num.statistics.kdeapp.kde2d.command.ArgumentRequiringCommand
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
-
-import matsu.num.statistics.kdeapp.kde2d.exception.InvalidParameterException;
 
 /**
  * {@link ArgumentRequiringCommand} のテスト.
@@ -25,29 +27,45 @@ import matsu.num.statistics.kdeapp.kde2d.exception.InvalidParameterException;
 @RunWith(Enclosed.class)
 final class ArgumentRequiringCommandTest {
 
+    public static final Class<?> TEST_CLASS = ArgumentRequiringCommand.class;
+
     public static class オプションコマンドの集合生成に関するテスト {
 
         @Test
-        public void test_INPUT_FILE_PATHを含むことを確かめる() {
-            assertThat(values(), containsInRelativeOrder(INPUT_FILE_PATH));
+        public void test_サイズが1以上を確かめ文字列列挙() {
+            assertThat(values().size(), is(greaterThan(0)));
+
+            System.out.println(TEST_CLASS.getName());
+            values().stream()
+                    .forEach(System.out::println);
+            System.out.println();
         }
     }
 
-    public static class オプションコマンドの文字列解釈のテスト {
+    @RunWith(Theories.class)
+    public static class オプションコマンドの文字列解釈の正常系テスト {
+
+        @DataPoints
+        public static Collection<ArgumentRequiringCommand<?>> commands = values();
+
+        @Theory
+        public void test_各コマンドについて解釈を実行(ArgumentRequiringCommand<?> command) {
+            assertThat(
+                    interpret(command.commandString()).get(),
+                    is(command));
+            for (String commandStr : command.expressions()) {
+                assertThat(
+                        interpret(commandStr).get(),
+                        is(command));
+            }
+        }
+    }
+
+    public static class オプションコマンドの文字列解釈の異常系テスト {
 
         @Test(expected = NullPointerException.class)
-        public void test_nullは不可() {
+        public void test_nullの場合() {
             interpret(null);
-        }
-
-        @Test
-        public void test_セパレータコマンド() {
-            assertThat(
-                    interpret("--separator-in").get(),
-                    is(SEPARATOR_INPUT));
-            assertThat(
-                    interpret("-sep-i").get(),
-                    is(SEPARATOR_INPUT));
         }
 
         @Test
@@ -55,19 +73,6 @@ final class ArgumentRequiringCommandTest {
             assertThat(
                     interpret("--unknown"),
                     is(Optional.empty()));
-        }
-    }
-
-    public static class オプションコマンドのコンバータのバリデーションテスト {
-
-        @Test(expected = InvalidParameterException.class)
-        public void test_セパレータinは空文字は不可() {
-            SEPARATOR_INPUT.convertArg("");
-        }
-
-        @Test(expected = InvalidParameterException.class)
-        public void test_セパレータoutは空文字は不可() {
-            SEPARATOR_OUTPUT.convertArg("");
         }
     }
 }
