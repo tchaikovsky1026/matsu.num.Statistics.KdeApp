186f83f27f71af42f74589bcd6701f3e91016213
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-02-22 21:18:37 +0900

    Feature/issue-57 InputFormat 入力フォーマットの契約を整理しパーサーを整備 (#58)
    * modify: コメント行評価と末尾空白削除のためのLineFilterと,データ抽出のLineParserを分ける
    
    * modify: データ抽出のDelimitedLineParserを分ける
    
    * docs: update history

--- src/matsu/num/statistics/kdeapp/format/DelimitedLineParser.java
+++ src/matsu/num/statistics/kdeapp/format/DelimitedLineParser.java
@@ -6,7 +6,7 @@
  */
 
 /*
- * 2026.2.21
+ * 2026.2.22
  */
 package matsu.num.statistics.kdeapp.format;
 
@@ -19,7 +19,7 @@ import java.util.regex.Pattern;
 
 /**
  * 固定カラム数のデータを区切り文字で結合した1行文字列に対し,
- * 区切り文字による分割とフィルタを行うクラス.
+ * フィルタと区切り文字による分割, データ抽出を行うクラス.
  * 
  * <p>
  * 入力データとして与えられる各行の文字列を想定している.
@@ -29,7 +29,7 @@ import java.util.regex.Pattern;
  */
 public final class DelimitedLineParser {
 
-    private final LineFilter lineFilter;
+    private final CommentLineFilter lineFilter;
 
     private final Separator separator;
     private final String separatorPattern;
@@ -57,7 +57,7 @@ public final class DelimitedLineParser {
 
         this.columns = columns;
         this.separator = Objects.requireNonNull(separator);
-        this.lineFilter = new LineFilter(commentPrefix);
+        this.lineFilter = new CommentLineFilter(commentPrefix);
 
         this.separatorPattern = Pattern.quote(this.separator.asString());
     }
@@ -72,38 +72,35 @@ public final class DelimitedLineParser {
     }
 
     /**
-     * 与えられた文字列に対してフィルタを実行する:
-     * {@link String} の配列として抽出する.
+     * 与えられた文字列に対してフィルタとデータ抽出を実行する:
      * 
      * <p>
-     * 前処理として, 前後のブランクが削除され, コメント判定される. <br>
+     * 前処理として, 末尾 white space が削除され, コメント判定される. <br>
      * 前処理の結果が空文字の場合, コメント開始文字列から始まる場合は空が返る. <br>
-     * その後, セパレータで分割され, 各要素について前後のブランクを削除する
-     * (処理の結果, ブランクになった場合は無視される).
+     * その後, セパレータで分割され, 各要素について前後の white space を削除する.
      * </p>
      * 
      * <p>
-     * フィルタの結果で, 要素の数が {@link #columns()} に一致しない場合は例外をスローする.
+     * データ抽出の結果で, 要素の数が {@link #columns()} に一致しない場合,
+     * 空データがある場合は例外をスローする.
      * </p>
      * 
      * @param line 解析する文字列
-     * @return エスケープ行, 空行なら空
+     * @return コメント行, 空行なら空
      * @throws IllegalArgumentException 正当なフォーマットでない場合
-     * @throws NullPointerException null
+     * @throws NullPointerException 引数がnullの場合
      */
     public Optional<String[]> apply(String line) {
 
         Optional<String[]> out = lineFilter.apply(line)
                 .map(s -> s.split(separatorPattern))
-                .map(
-                        arr -> Arrays.stream(arr)
-                                .flatMap(s -> lineFilter.apply(s).stream())
-                                .toArray(String[]::new));
+                .map(s -> toData(s));
 
-        // カラム数の確認
+        // カラム数, 空データの確認
         out.ifPresent(arr -> {
-            if (arr.length != columns) {
-                throw new IllegalArgumentException("invalid column size: " + line);
+            if (arr.length != columns ||
+                    Arrays.stream(arr).anyMatch(String::isEmpty)) {
+                throw new IllegalArgumentException("invalid line: \"" + line + "\"");
             }
         });
 
@@ -111,21 +108,48 @@ public final class DelimitedLineParser {
     }
 
     /**
-     * 与えられた文字列に対してフィルタを実行し,
+     * ソースの文字列配列をデータ化する. <br>
+     * データ化は, 以下を行う.
+     * 
+     * <ul>
+     * <li>文字列の両側 white space を削除.</li>
+     * <li>末尾にある連続した空文字をすべて排除.</li>
+     * <li>末尾でない位置に空文が残っている場合は残す.</li>
+     * </ul>
+     */
+    private String[] toData(String[] src) {
+
+        int validSize = src.length;
+
+        // strip後に空にならないデータを探すことで, 有効なサイズを知る
+        while (validSize >= 1) {
+            if (!src[validSize - 1].strip().isEmpty()) {
+                break;
+            }
+            validSize--;
+        }
+
+        return Arrays.stream(src)
+                .limit(validSize)
+                .map(String::strip)
+                .toArray(String[]::new);
+    }
+
+    /**
+     * 与えられた文字列に対してデータ抽出を実行し,
      * mapper によって値を変換して返す.
      * 
      * <p>
      * {@link #apply(String)} の要件に従う. <br>
-     * また, 空文字でない場合で, mapper によって例外が発生した場合はそのままスローする.
+     * また, 戻り値が空でない場合で, mapper によって例外が発生した場合はそのままスローする.
      * </p>
      * 
      * @param <T> マッパによる変換後の型
      * @param line 文字列
      * @param mapper マッパ
      * @param typeToken {@code T} 型の型トークン
-     * @return フィルタされた結果
+     * @return 抽出された結果
      * @throws RuntimeException マッパが例外をスローした場合
-     * @throws IllegalArgumentException 正当なフォーマットでない場合
      * @throws NullPointerException 文字列がnullの場合, フィルタの結果が空でなくマッパがnullの場合
      */
     public <T> Optional<T[]> apply(
--- test/matsu/num/statistics/kdeapp/format/DelimitedLineParserTest.java
+++ test/matsu/num/statistics/kdeapp/format/DelimitedLineParserTest.java
@@ -10,6 +10,8 @@ package matsu.num.statistics.kdeapp.format;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Optional;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
@@ -27,12 +29,17 @@ final class DelimitedLineParserTest {
 
         @Before
         public void before_パーサーの用意() {
-            parser = new DelimitedLineParser(2, Separator.from(","), CommentPrefix.of("#"));
+            parser = new DelimitedLineParser(2, Separator.from(","), CommentPrefix.of("//"));
+        }
+
+        @Test
+        public void test_スラッシュエスケープ() {
+            assertThat(parser.apply("// dummy"), is(Optional.empty()));
         }
 
         @Test
         public void test_Doubleへの変換() {
-            String line = "1.0,2d";
+            String line = "  1.0   ,  2d   ";
             Double[] result =
                     parser.apply(line, Double::parseDouble, Double.class)
                             .get();
@@ -44,7 +51,7 @@ final class DelimitedLineParserTest {
 
         @Test
         public void test_Doubleへの変換_後ろに空があり() {
-            String line = "1.0,3d,";
+            String line = "   1.0 ,  3d , ,";
             Double[] result =
                     parser.apply(line, Double::parseDouble, Double.class)
                             .get();
