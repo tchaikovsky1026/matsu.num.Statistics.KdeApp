366646d4792df9f7d272c3ae088bd8827f83ed5c
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-03-07 22:19:13 +0900

    feat: xuz-blockでラベル行を出力できるようにする.

--- src/matsu/num/statistics/kdeapp/kde2d/XyzBlockTypeFormatterBuilder.java
+++ src/matsu/num/statistics/kdeapp/kde2d/XyzBlockTypeFormatterBuilder.java
@@ -6,15 +6,20 @@
  */
 
 /*
- * 2026.3.5
+ * 2026.3.7
  */
 package matsu.num.statistics.kdeapp.kde2d;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 
+import matsu.num.statistics.kdeapp.base.IterableFlattening;
 import matsu.num.statistics.kdeapp.format.Separator;
 import matsu.num.statistics.kdeapp.kde2d.format.BlockFlattening;
 import matsu.num.statistics.kerneldensity.output.FormattableKdeResult2D;
+import matsu.num.statistics.kerneldensity.output.Kde2dCharSVTextFormatter;
 import matsu.num.statistics.kerneldensity.output.Kde2dFormatter;
 import matsu.num.statistics.kerneldensity.output.StructuredCharSVTextFormatter;
 
@@ -28,12 +33,14 @@ final class XyzBlockTypeFormatterBuilder {
 
     private volatile Separator separator;
     private volatile int blankGap;
+    private volatile String labelPrefix;
 
     /**
      * 区切り文字を与えて, ビルダインスタンスを立ち上げる.
      * 
      * <p>
-     * デフォルトは, blankGap = 1 である.
+     * デフォルトは, blankGap = 1,
+     * ラベル無しである.
      * </p>
      * 
      * @param separator 区切り文字
@@ -42,6 +49,7 @@ final class XyzBlockTypeFormatterBuilder {
     XyzBlockTypeFormatterBuilder(Separator separator) {
         this.separator = Objects.requireNonNull(separator);
         blankGap = 1;
+        labelPrefix = null;
     }
 
     /**
@@ -86,27 +94,105 @@ final class XyzBlockTypeFormatterBuilder {
     }
 
     /**
+     * ラベル出力に関し, 出力する設定に変更する
+     * (ラベルの先頭に付与する文字を与える).
+     * 
+     * <p>
+     * <i>
+     * {@code this}
+     * をリターンするので注意.
+     * </i>
+     * </p>
+     * 
+     * @param labelPrefix ラベルの先頭に付与する文字
+     * @return {@code this}
+     */
+    XyzBlockTypeFormatterBuilder enableLabel(char labelPrefix) {
+        return this.enableLabel(String.valueOf(labelPrefix));
+    }
+
+    /**
+     * ラベル出力に関し, 出力する設定に変更する
+     * (ラベルの先頭に付与する文字列を与える).
+     * 
+     * <p>
+     * <i>
+     * {@code this}
+     * をリターンするので注意.
+     * </i>
+     * </p>
+     * 
+     * @param labelPrefix ラベルの先頭に付与する文字列
+     * @return {@code this}
+     * @throws NullPointerException 引数がnullの場合
+     */
+    XyzBlockTypeFormatterBuilder enableLabel(String labelPrefix) {
+        this.labelPrefix = Objects.requireNonNull(labelPrefix);
+        return this;
+    }
+
+    /**
+     * {@code this} のラベル出力に関し, 出力しない設定に変更する.
+     * 
+     * <p>
+     * <i>
+     * {@code this}
+     * をリターンするので注意.
+     * </i>
+     * </p>
+     * 
+     * @return {@code this}
+     */
+    XyzBlockTypeFormatterBuilder disableLabel() {
+        this.labelPrefix = null;
+        return this;
+    }
+
+    /**
      * フォーマッターをビルドする.
      * 
      * @return フォーマッター
      */
     WritingFormatter build() {
-        return createFormatter(separator, blankGap);
+        return createFormatter(separator, blankGap, labelPrefix);
     }
 
     /**
      * フォーマッターを生成するstaticメソッド.
      * build メソッドから呼ばれることを想定.
+     * 
+     * <p>
+     * labelPrefix が null の場合はラベル出力しない.
+     * </p>
      */
-    private static WritingFormatter createFormatter(Separator separator, int blankGap) {
+    private static WritingFormatter createFormatter(Separator separator, int blankGap, String labelPrefix) {
         Kde2dFormatter<Iterable<Iterable<String>>> innerFormatter =
                 StructuredCharSVTextFormatter.of(separator.charValue());
         BlockFlattening flattening = new BlockFlattening(blankGap);
 
+        // ラベル行を作成するための関数
+        /*
+         * Kde2dCharSVTextFormatter を使い, 1行目だけを出力することでラベル行を取り出す.
+         */
+        Function<FormattableKdeResult2D, Iterable<String>> labelStringCreator =
+                Objects.isNull(labelPrefix)
+                        ? result -> Collections.emptyList()
+                        : result -> {
+                            String label = result.formatted(
+                                    Kde2dCharSVTextFormatter.withLabelEscaped(
+                                            separator.charValue(), labelPrefix))
+                                    .iterator().next();
+                            return List.of(label);
+                        };
+
         return new WritingFormatter() {
             @Override
             public Iterable<String> format(FormattableKdeResult2D kde2dResult) {
-                return flattening.apply(kde2dResult.formatted(innerFormatter));
+
+                return IterableFlattening.flatten(
+                        List.of(
+                                labelStringCreator.apply(kde2dResult),
+                                flattening.apply(kde2dResult.formatted(innerFormatter))));
             }
         };
     }
