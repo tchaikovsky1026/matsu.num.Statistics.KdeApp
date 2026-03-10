d68fad1d9b4d413c6fbaa7f6483aaa73622e4983
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-03-10 22:50:01 +0900

    add: kde1d.taskパッケージ

---
+++ src/matsu/num/statistics/kdeapp/kde1d/task/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * kde1d に関係した, 処理の要素を扱う.
+ */
+package matsu.num.statistics.kdeapp.kde1d.task;
