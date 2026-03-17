4bce96601152742d0db46fcc8b8cd68c9ffeeaf2
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-03-17 13:37:31 +0900

    add: kdeapp.helpパッケージの用意

---
+++ src/matsu/num/statistics/kdeapp/help/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * アプリケーションのヘルプの提供に関わるパッケージ.
+ */
+package matsu.num.statistics.kdeapp.help;
