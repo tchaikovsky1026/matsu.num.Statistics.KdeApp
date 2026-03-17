d5f03d9f156a7db322916bfe956fee4279cff10f
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-03-17 13:40:21 +0900

    modify: 既存クラスのアクセス修飾子の緩和

--- test/matsu/num/statistics/kdeapp/command/DummyCommandListForTesting.java
+++ test/matsu/num/statistics/kdeapp/command/DummyCommandListForTesting.java
@@ -6,7 +6,7 @@
  */
 
 /*
- * 2026.2.19
+ * 2026.3.17
  */
 package matsu.num.statistics.kdeapp.command;
 
@@ -15,29 +15,29 @@ package matsu.num.statistics.kdeapp.command;
  * 
  * @author Matsuura Y.
  */
-final class DummyCommandListForTesting {
+public final class DummyCommandListForTesting {
 
-    static final NoArgumentCommand DUMMY_NO_ARG_1 =
+    public static final NoArgumentCommand DUMMY_NO_ARG_1 =
             NoArgumentCommand.of(
                     "DUMMY_NO_ARG_1", "--dummy-no-arg-1", "-d-na-1");
 
-    static final NoArgumentCommand DUMMY_NO_ARG_2 =
+    public static final NoArgumentCommand DUMMY_NO_ARG_2 =
             NoArgumentCommand.of(
                     "DUMMY_NO_ARG_2", "--dummy-no-arg-2", "-d-na-2");
 
-    static final NoArgumentCommand DUMMY_NO_ARG_3 =
+    public static final NoArgumentCommand DUMMY_NO_ARG_3 =
             NoArgumentCommand.of(
                     "DUMMY_NO_ARG_3", "--dummy-no-arg-3", "-d-na-3");
 
-    static final ArgumentRequiringCommand<?> DUMMY_ARG_1 =
+    public static final ArgumentRequiringCommand<?> DUMMY_ARG_1 =
             ArgumentRequiringCommand.identifying(
                     "DUMMY_ARG_1", "--dummy-arg-1", "-d-a-1");
 
-    static final ArgumentRequiringCommand<?> DUMMY_ARG_2 =
+    public static final ArgumentRequiringCommand<?> DUMMY_ARG_2 =
             ArgumentRequiringCommand.identifying(
                     "DUMMY_ARG_2", "--dummy-arg-2", "-d-a-2");
 
-    static final ArgumentRequiringCommand<?> DUMMY_ARG_3 =
+    public static final ArgumentRequiringCommand<?> DUMMY_ARG_3 =
             ArgumentRequiringCommand.identifying(
                     "DUMMY_ARG_3", "--dummy-arg-3", "-d-a-3");
 
