9e9e45a1f6cb06f6930199f3d09191916a8cf5f1
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2026-03-18 14:26:20 +0900

    feat: kde1d-hのエントリーポイントの用意

---
+++ test/matsu/num/statistics/kdeapp/kde1d/help/Kde1dHelpDisplayRunnerTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+package matsu.num.statistics.kdeapp.kde1d.help;
+
+import org.junit.Test;
+import org.junit.Test.None;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+
+/**
+ * {@link Kde1dHelpDisplayRunner } のテスト.
+ */
+@RunWith(Enclosed.class)
+final class Kde1dHelpDisplayRunnerTest {
+
+    public static final Class<?> TEST_CLASS = Kde1dHelpDisplayRunner.class;
+
+    public static class ランナーの実行 {
+
+        @Test(expected = None.class)
+        public void test_ヘルプの表示() {
+            System.out.println(TEST_CLASS.getName());
+            System.out.println("=================");
+            new Kde1dHelpDisplayRunner().run();
+            System.out.println("=================");
+            System.out.println();
+        }
+    }
+}
