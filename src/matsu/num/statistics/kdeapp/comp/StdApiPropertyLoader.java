c38790a94a47022271a70c227ca7f0c41d2bd0e2
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-04-09 23:45:36 +0900

    Feature/issue-109-loader 標準APIのPropertiesを用いたResolver生成ファサードの整備 (#119)
    * refactor: PropertyContainer
    
    * feat,test: StdApiPropertyLoader,テスト

---
+++ src/matsu/num/statistics/kdeapp/comp/StdApiPropertyLoader.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2026.4.9
+ */
+package matsu.num.statistics.kdeapp.comp;
+
+import java.util.Set;
+
+/**
+ * {@link java.util.Properties} からプロパティを読み込み,
+ * Resolver を構築する仕組み.
+ * 
+ * @author Matsuura Y.
+ */
+public final class StdApiPropertyLoader {
+
+    private final Set<PropertyKey> preparedKeys;
+    private final Set<ResolverDesign<?>> resolverDesigns;
+
+    /**
+     * ローダーを構築する.
+     * 
+     * <p>
+     * 構築時に, このローダーが扱うプロパティキーの候補と,
+     * Resolver の設計図を渡す.
+     * </p>
+     * 
+     * @param preparedKeys 候補プロパティキーのセット
+     * @param resolverDesigns Resolver の設計図のセット
+     * @throws NullPointerException 引数にnullを含む場合
+     */
+    public StdApiPropertyLoader(Set<PropertyKey> preparedKeys, Set<ResolverDesign<?>> resolverDesigns) {
+        super();
+        this.preparedKeys = Set.copyOf(preparedKeys);
+        this.resolverDesigns = Set.copyOf(resolverDesigns);
+    }
+
+    /**
+     * 与えたプロパティ ({@link java.util.Properties})
+     * から ResolverContainer を構築する.
+     * 
+     * <p>
+     * 与えたプロパティが, 候補に登録されていないキーを含む場合,
+     * あるいは, 設計図に対して, 必要なプロパティが不足, または値が不正, の場合は
+     * {@link IllegalArgumentException} をスローする
+     * (例外メッセージは意味のある文字列である). <br>
+     * ただし, 設計図に対して必要なプロパティを全く含まない場合は
+     * Container に登録されないのみで例外はスローされない.
+     * </p>
+     * 
+     * @param properties プロパティ
+     * @return プロパティから構築された ResolverContainer
+     * @throws IllegalArgumentException プロパティが不正の場合 (メソッド説明文)
+     * @throws NullPointerException 引数がnullの場合
+     */
+    public ResolverContainer load(java.util.Properties properties) {
+        var propertyContainer = new PropertyContainer.StdApiReader(preparedKeys)
+                .convert(properties);
+        return propertyContainer.toResolvers(resolverDesigns);
+    }
+}
