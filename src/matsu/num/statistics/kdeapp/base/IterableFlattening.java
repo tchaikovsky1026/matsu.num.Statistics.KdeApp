f7d60b3b98629548bbf6147928220d10989a2a4c
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-04-14 18:28:03 +0900

    Feature/refactor-base kdeapp.baseパッケージ内コンポーネントのリファクタ (#127)
    * refactor IterableFlattening, test
    
    * refactor StandardPropertyLoader,(add) test
    
    * update comment in ThrowableSupplier
    
    * update comment in ConstantsCollector

--- src/matsu/num/statistics/kdeapp/base/IterableFlattening.java
+++ src/matsu/num/statistics/kdeapp/base/IterableFlattening.java
@@ -6,7 +6,7 @@
  */
 
 /*
- * 2026.3.7
+ * 2026.4.14
  */
 package matsu.num.statistics.kdeapp.base;
 
@@ -27,34 +27,31 @@ public final class IterableFlattening {
     }
 
     /**
-     * 与えられたネスト {@link Iterable} をフラット化する.
+     * 与えられたネスト {@code Iterable} をフラット化する.
      * 
      * <p>
-     * 得られたイテレータはスレッドセーフでない. <br>
-     * remove メソッドはサポートされていない.
+     * 得られた {@code Iterable} のイテレータは, スレッドセーフでない. <br>
+     * また, remove メソッドはサポートされていない.
      * </p>
      * 
      * <p>
      * 外側イテレータの要素に null が含まれる場合,
      * イテレータ時に例外がスローされる. <br>
      * イテレータ中に,
-     * ソースとなるネスト {@link Iterable} の内部状態が書き換えられることは想定されていない. <br>
-     * 動作は保証されない.
+     * ソースとなるネスト {@code Iterable} の内部状態が書き換えられることは想定されていない. <br>
+     * 書き換えられた場合の動作は保証されない.
      * </p>
      * 
      * @param <T> 要素の型
-     * @param src ソースとなるネストされた {@link Iterable}
-     * @return フラット化の結果
+     * @param src ソースとなるネストされた {@code Iterable}
+     * @return フラット化された {@code Iterable}
      * @throws NullPointerException 引数が null の場合
      */
     public static <T> Iterable<T> flatten(Iterable<? extends Iterable<? extends T>> src) {
         return new FlattenedIterable<T>(src);
     }
 
-    /**
-     * フラット化された Iterator の実装. <br>
-     * スレッドセーフでない.
-     */
+    /** フラット化された {@code Iterable} の実装. (イテレータはスレッドセーフでなく, remove 不可) */
     private static final class FlattenedIterable<T> implements Iterable<T> {
 
         private final Iterable<? extends Iterable<? extends T>> src;
@@ -72,10 +69,15 @@ public final class IterableFlattening {
             return new FlattenedIterator();
         }
 
+        /** フラット化イテレータ */
         private final class FlattenedIterator implements Iterator<T> {
 
             private final Iterator<? extends Iterable<? extends T>> outer = src.iterator();
             private Iterator<? extends T> inner = Collections.emptyIterator();
+
+            /** 唯一のコンストラクタ. */
+            FlattenedIterator() {
+            }
 
             @Override
             public boolean hasNext() {
@@ -90,10 +92,16 @@ public final class IterableFlattening {
             }
 
             /**
-             * inner.hasNext == true となるまでイテレータを進める.
-             * ただし, outer, inner ともに hasNext == false となった場合は終了.
+             * ネストイテレータの状態を正規化する. <br>
+             * 正規化された状態とは, "残要素がある" と
+             * {@code inner.hasNext == true}
+             * が等価である状態である.
              */
             private void normalize() {
+                /*
+                 * inner.hasNext == true となるまでイテレータを進める.
+                 * ただし, outer, inner ともに hasNext == false となった場合は終了.
+                 */
                 while (!inner.hasNext()) {
                     if (!outer.hasNext()) {
                         break;
